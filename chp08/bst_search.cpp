178e1d40d336595a0a962502e64021931dbac8b8
pengchant <13961826751@163.com>
2025-01-15 11:28:48 +0800

    完成二叉排序树-创建二叉排序树销毁二叉排序树

--- chp08/bst_search.cpp
+++ chp08/bst_search.cpp
@@ -24,3 +24,52 @@ BSTNode *bstSearch(BSTNode *bt, KeyType k) {
     }
     return NULL;
 }
+
+
+int bstInsert(BSTNode *&bt, KeyType k) {
+    BSTNode *f; // f 缓存插入结点的指针
+    BSTNode *p = bt;
+    while (p != NULL) {
+        if (p->key == k) {
+            return 0;
+        }
+        f = p; // f 指向 p 结点的双亲结点
+        if (k < p->key) {
+            p = p->lchild;
+        } else {
+            p = p->rchild;
+        }
+    }
+    // 新建结点
+    p = (BSTNode *) malloc(sizeof(BSTNode));
+    p->key = k;
+    p->lchild = p->rchild = NULL;
+    // 然后再对应的位置插入结点
+    if (bt == NULL) {
+        bt = p;
+    } else if (k < f->key) {
+        f->lchild = p;
+    } else {
+        f->rchild = p;
+    }
+    return 1;
+}
+
+// createBST 创建二叉树运算算法
+void createBST(BSTNode *&bt, KeyType a[], int n) {
+    bt = NULL;
+    int i = 0;
+    while (i < n) {
+        bstInsert(bt, a[i]); // 将关键字a[i]插入二叉树bt中
+        i++;
+    }
+}
+
+void destroyBST(BSTNode *&bt) {
+    if (bt == NULL) {
+        return;
+    }
+    destroyBST(bt->lchild);
+    destroyBST(bt->rchild);
+    free(bt);
+}
